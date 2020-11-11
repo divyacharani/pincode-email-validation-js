d8b200f07f5ceb094367fdbf901476a63b56c48a
unknown <divyacharaninabha@gmail.com>
2020-11-11 22:41:59 +0530

    Added: Functionality to validate pincode added

--- PincodeEmailValidation.js
+++ PincodeEmailValidation.js
@@ -1,2 +1,13 @@
-// Welcome message
-console.log("Welcome!!");
+let validatePincode = (pincode) => {
+    let pincodeRegex = RegExp("^[1-9]{1}[0-9]{5}");
+    if (pincodeRegex.test(pincode))
+        return "Valid Pincode!!";
+    else throw "Invalid Pincode!!";
+};
+
+try {
+    console.log("Is 400088 pincode Valid : " + validatePincode("400088"));
+}
+catch (error) {
+    console.error(error);
+}
