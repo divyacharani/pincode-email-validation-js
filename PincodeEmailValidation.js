69935e9cc83f99fb7949b2dbb62d00fe95214590
unknown <divyacharaninabha@gmail.com>
2020-11-11 23:18:40 +0530

    Refactored: Functionality to validate pincode refactored

--- PincodeEmailValidation.js
+++ PincodeEmailValidation.js
@@ -5,40 +5,22 @@ let validatePincode = (pincode) => {
     else throw "Invalid Pincode!!";
 };
 
-// Create a regex pattern to validate PIN code 400088 
-console.log("Is 400088 pincode Valid : ");
-try {
+let checkPincode = (pincode) => {
+    console.log("Is " + pincode + " pincode Valid : ");
+    try {
 
-    console.log(validatePincode("400088"));
-}
-catch (error) {
-    console.error(error);
-}
+        console.log(validatePincode(pincode));
+    }
+    catch (error) {
+        console.error(error);
+    }
+};
 
-// Restrict the PIN code from taking alphabets or special characters at the beginning
-console.log("Is A400088 pincode Valid : ");
-try {
-    console.log(validatePincode("A400088"));
-}
-catch (error) {
-    console.error(error);
-}
+checkPincode("400088");         // Create a regex pattern to validate PIN code 400088 
 
-// Restrict the PIN code from taking alphabets or special characters at the End
-console.log("Is 400088B pincode Valid : ");
-try {
-    console.log(validatePincode("400088B"));
-}
-catch (error) {
-    console.error(error);
-}
+checkPincode("A400088");        // Restrict the PIN code from taking alphabets or special characters at the beginning
 
-// Make sure 400 088 is also valid along with 400088
-console.log("Is 400 088 pincode Valid : ");
-try {
-    console.log(validatePincode("400 088"));
-}
-catch (error) {
-    console.error(error);
-}
+checkPincode("400088B");        // Restrict the PIN code from taking alphabets or special characters at the End
+
+checkPincode("400 088");        // Make sure 400 088 is also valid along with 400088
 
